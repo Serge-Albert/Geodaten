f36c1ea0a2300f9ae9c7724b4f049f3b388bc01e
16.Msk.01 <a.falmann@googlemail.com>
2024-03-01 16:10:02 +0100

    first commit

---
+++ geolocated_map.js
@@ -0,0 +1,23 @@
+ymaps.ready(function () {
+    var map;
+    ymaps.geolocation.get().then(function (res) {
+        var mapContainer = $('#map'),
+            bounds = res.geoObjects.get(0).properties.get('boundedBy'),
+            // Рассчитываем видимую область для текущей положения пользователя.
+            mapState = ymaps.util.bounds.getCenterAndZoom(
+                bounds,
+                [mapContainer.width(), mapContainer.height()]
+            );
+        createMap(mapState);
+    }, function (e) {
+        // Если местоположение невозможно получить, то просто создаем карту.
+        createMap({
+            center: [55.692293896284056, 37.604548931121826],
+            zoom: 16
+        });
+    });
+
+    function createMap(state) {
+        map = new ymaps.Map('map', state);
+    }
+});
